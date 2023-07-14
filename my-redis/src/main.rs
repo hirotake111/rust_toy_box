836a7a7fa20eb986d8c1a5da90bc9495ea48b643
hirotake111 <hirotake111@users.noreply.github.com>
2023-07-14 23:27:33 +0900

    update: my-redis/simple implementation to handle concurrent requests

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,10 +1,46 @@
-use mini_redis::{client, Result};
+use std::collections::HashMap;
+
+use mini_redis::Command::{self, Get, Set};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("got value from the server; result={:?}", result);
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let _ = process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) -> Result<()> {
+    let mut db = HashMap::new();
+    let mut connection = Connection::new(socket);
+    while let Some(frame) = connection.read_frame().await? {
+        println!("GOT: {:?}", frame);
+        let response = match Command::from_frame(frame)? {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`.
+                    // This type will be covered later in the tutorial.
+                    // For now, `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+        connection.write_frame(&response).await?;
+    }
+    println!("done processing.");
     Ok(())
 }
