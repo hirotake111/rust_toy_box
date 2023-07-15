3c07db3e58a6e3683c7deab3858b4d865a5a8f88
hirotake111 <hirotake111@users.noreply.github.com>
2023-07-15 22:30:35 +0900

    update: my-redis/refactor

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -9,9 +9,42 @@ use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-#[allow(dead_code)]
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
+// type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+struct Sharded(Mutex<HashMap<String, Bytes>>);
+
+impl Sharded {
+    fn new() -> Self {
+        Self(Mutex::new(HashMap::new()))
+    }
+
+    fn insert(&self, key: String, value: Bytes) -> Frame {
+        match self.0.lock() {
+            Ok(mut shard) => {
+                shard.insert(key, value);
+                Frame::Simple("OK".to_string())
+            }
+            Err(err) => Frame::Error(format!("Failed obtaining lock: {:?}", err).to_string()),
+        }
+    }
+
+    fn get(&self, key: &str) -> Frame {
+        match self.0.lock() {
+            Ok(shard) => {
+                if let Some(value) = shard.get(key) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`.
+                    // This type will be covered later in the tutorial.
+                    // For now, `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            Err(err) => Frame::Error(format!("Failed obtaining lock: {:?}", err).to_string()),
+        }
+    }
+}
+
+type Db = Arc<Vec<Sharded>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -33,7 +66,7 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn process(socket: TcpStream, db: ShardedDb) -> Result<()> {
+async fn process(socket: TcpStream, db: Db) -> Result<()> {
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await? {
         println!("GOT: {:?}", frame);
@@ -42,35 +75,13 @@ async fn process(socket: TcpStream, db: ShardedDb) -> Result<()> {
                 let sharded_key = calculate_hash(&cmd.key()) % db.len();
                 println!("sharded_key: {sharded_key}");
                 let shard = &db[sharded_key];
-                match shard.lock() {
-                    Ok(mut shard) => {
-                        shard.insert(cmd.key().to_string(), cmd.value().clone());
-                        Frame::Simple("OK".to_string())
-                    }
-                    Err(err) => {
-                        Frame::Error(format!("Failed obtaining lock: {:?}", err).to_string())
-                    }
-                }
+                shard.insert(cmd.key().to_string(), cmd.value().clone())
             }
             Get(cmd) => {
                 let sharded_key = calculate_hash(&cmd.key()) % db.len();
                 println!("sharded_key: {sharded_key}");
                 let shard = &db[sharded_key];
-                match shard.lock() {
-                    Ok(shard) => {
-                        if let Some(value) = shard.get(cmd.key()) {
-                            // `Frame::Bulk` expects data to be of type `Bytes`.
-                            // This type will be covered later in the tutorial.
-                            // For now, `&Vec<u8>` is converted to `Bytes` using `into()`.
-                            Frame::Bulk(value.clone().into())
-                        } else {
-                            Frame::Null
-                        }
-                    }
-                    Err(err) => {
-                        Frame::Error(format!("Failed obtaining lock: {:?}", err).to_string())
-                    }
-                }
+                shard.get(cmd.key())
             }
             cmd => panic!("unimplemented {:?}", cmd),
         };
@@ -79,10 +90,11 @@ async fn process(socket: TcpStream, db: ShardedDb) -> Result<()> {
     Ok(())
 }
 
-fn new_sharded_db(num_shards: usize) -> ShardedDb {
+fn new_sharded_db(num_shards: usize) -> Db {
     let mut db = Vec::with_capacity(num_shards);
     for _ in 0..num_shards {
-        db.push(Mutex::new(HashMap::new()));
+        // db.push(Mutex::new(HashMap::new()));
+        db.push(Sharded::new());
     }
     Arc::new(db)
 }
